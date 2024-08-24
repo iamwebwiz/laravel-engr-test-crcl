0201453262a83b482685fd19eb6e9490986cc031
Ezekiel <iamwebwiz@gmail.com>
2024-08-24 23:22:03 +0100

    Add mass-assignable fields and casts to `Order` model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'hmo_id',
+        'batch_id',
+        'provider_name',
+        'encounter_date',
+        'total',
+    ];
+
+    protected $casts = [
+        'encounter_date' => 'date',
+    ];
 }
