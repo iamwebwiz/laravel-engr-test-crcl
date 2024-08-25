a03f30ab96b28d53106e79409267ac6ebea7c99c
Ezekiel <iamwebwiz@gmail.com>
2024-08-25 09:13:20 +0100

    Implementation of batching service

--- app/Modules/Batching/Services/BatchingService.php
+++ app/Modules/Batching/Services/BatchingService.php
@@ -4,6 +4,8 @@ namespace App\Modules\Batching\Services;
 
 use App\Models\Order;
 use App\Modules\Batching\Enums\BatchingStrategyEnum;
+use App\Modules\Batching\Exceptions\HmoNotSetException;
+use App\Modules\Batching\Notifications\BatchOrderCreatedNotification;
 use App\Modules\Batching\Strategies\EncounterDateStrategy;
 use App\Modules\Batching\Strategies\SubmissionDateStrategy;
 use Lorisleiva\Actions\Concerns\AsAction;
@@ -32,8 +34,25 @@ class BatchingService
         ];
     }
 
-    public function handle(Order $order)
+    public function handle(Order $order): void
     {
-        //
+        $orderHmo = $order->hmo;
+
+        if (! $orderHmo) {
+            throw new HmoNotSetException('[Bad Order] - An HMO has not been set for this order.');
+        }
+
+        if ($orderHmo->batching_strategy && ! empty($this->strategies[$orderHmo->batching_strategy])) {
+            $strategy = $this->strategies[$orderHmo->batching_strategy];
+        } else {
+            $strategy = self::DEFAULT_STRATEGY;
+        }
+
+        $batch = app($strategy)->getBatch($order);
+
+        $order->batch()->associate($batch);
+        $order->save();
+
+        $orderHmo->notify(new BatchOrderCreatedNotification($order));
     }
 }
