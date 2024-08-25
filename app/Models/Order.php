b96e42107c5dbc4b177c1a47349186f51de023d2
Ezekiel <iamwebwiz@gmail.com>
2024-08-25 05:38:50 +0100

    Add relationships to Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
@@ -20,4 +22,19 @@ class Order extends Model
     protected $casts = [
         'encounter_date' => 'date',
     ];
+
+    public function hmo(): BelongsTo
+    {
+        return $this->belongsTo(Hmo::class);
+    }
+
+    public function batch(): BelongsTo
+    {
+        return $this->belongsTo(Batch::class);
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
